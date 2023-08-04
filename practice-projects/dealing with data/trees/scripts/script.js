1653b4872e054f4b08746a61d63f0c7ff00a9e1a
Brandon Johnson <bran.a.l.john@gmail.com>
2023-08-04 10:41:36 -0500

    Addes Script for trees

--- practice-projects/dealing with data/trees/scripts/script.js
+++ practice-projects/dealing with data/trees/scripts/script.js
@@ -5,3 +5,63 @@ class Node {
         this.right = null;
     }
 }
+
+class BinaryTree {
+    constructor(value){
+        this.root = null;
+    }
+
+    graft(value){
+        let current = this.root;
+        if(value === current.value) return undefined;
+        while(true){
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = value;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = value;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    find(value){
+        if(this.root === null) return undefined;
+        let current = this.root;
+        let found = false;
+        while(current && !found){
+            if(value < current){
+                current = current.left;
+            } else if(value > current){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+
+        if(!found) return undefined;
+        return current;
+    }
+
+    contains(value){
+        if(this.root === null) return undefined;
+        let current = this.root;
+        while(current){
+            if(value < current){
+                current = current.left;
+            } else if(value > current){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
