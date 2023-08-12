35bd7cb27bc72d0ec3887c2307a3f747aa74fed5
Brandon Johnson <bran.a.l.john@gmail.com>
2023-08-12 10:01:05 -0500

    Fixed the grafting function on Trees.

--- practice-projects/dealing with data/trees/scripts/script.js
+++ practice-projects/dealing with data/trees/scripts/script.js
@@ -13,17 +13,45 @@ class BinaryTree {
 
     graft(value){
         let current = this.root;
+        if(current === null){
+            this.root = new Node(value);
+            return this;
+        }
         while(true){
             if(value === current.value) return undefined;
             if(value < current.value){
                 if(current.left === null){
-                    current.left = value;
+                    current.left = new Node(value);
+                    return this;
+                }
+                current = current.left;
+            } else if(value > current.value){
+                if(current.right === null){
+                    current.right = new Node(value);
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    graftNode(preexistingNode){
+        let current = this.root;
+        if(current === null){
+            this.root = preexistingNode;
+            return this;
+        }
+        while(true){
+            if(preexistingNode.value === current.value) return undefined;
+            if(preexistingNode.value < current.value){
+                if(current.left === null){
+                    current.left = preexistingNode;
                     return this;
                 }
                 current = current.left;
             } else {
                 if(current.right === null){
-                    current.right = value;
+                    current.right = preexistingNode;
                     return this;
                 }
                 current = current.right;
@@ -36,9 +64,9 @@ class BinaryTree {
         let current = this.root;
         let found = false;
         while(current && !found){
-            if(value < current){
+            if(value < current.value){
                 current = current.left;
-            } else if(value > current){
+            } else if(value > current.value){
                 current = current.right;
             } else {
                 found = true;
@@ -53,9 +81,9 @@ class BinaryTree {
         if(this.root === null) return undefined;
         let current = this.root;
         while(current){
-            if(value < current){
+            if(value < current.value){
                 current = current.left;
-            } else if(value > current){
+            } else if(value > current.value){
                 current = current.right;
             } else {
                 return true;
--- practice-projects/games/2048/scripts/board.js
+++ practice-projects/games/2048/scripts/board.js
@@ -78,71 +78,41 @@ class Board{
     shiftTiles(direction){
         let x = 0;
         let y = 0;
+        let slideX = 0;
+        let slideY = 0;
         let done = false;
         switch(direction){
             case "up":
-                y = -1;
-                break;
-            case "left":
                 x = -1;
                 break;
+            case "left":
+                y = -1;
+                break;
             case "down":
-                y = 1;
+                x = 1;
                 break;
             case "right":
-                x = 1;
+                y = 1;
                 break;
         }
 
-        /*
-            ****************************************************************************************************************
-            Calculated what equation would allow i to be 0 when x is 0, increment from 1 when x = -1, or decrement from a variable when x = 1 using the following logic:
-
-            the line, i = (S - 1)x/2 + (S-1)/2 + 1, is evaluated at 1 when x = -1 and S when x = 1, where S = size
-            this can be multiplied by the equation x^2 which is 1 when x = -1 and x = 1, and is 0 when x = 0.
-            The end result is the following equation where i(-1) = 1, i(0) = 0, and i(1) = S:
-
-            i(x) = (S - 1)(x^3)/2 + (S + 1)(x^2)/2
-
-            1 is subtracted from S everywhere to account for proper indexing.
-            i is decremented by x in order to move in the proper direction.
-
-            ****************************************************************************************************************
-
-            A similar equation was bult for the increment where i must increment by 1 when x is -1 and 0 and increment by -1 when x is 1.
-            A quadratic equation is sufficient to handle all three points so the following logic is applied:
-
-            f(x) = ax^2 + bx + c
-            f(0) = 1, therefore c = 1
-            f(-1) = 1 and f(1) = -1, therefore 
-                a - b + 1 = 1   and 
-                a + b + 1 = -1
-            from here we get, a = -2 and b = 2
-
-            the final equation is:
-
-            f(x) = -2(x^2) + 2x + 1
-
-            this is subtracted from i every iteration.
-            Everything above is the same for j and y.
-            ****************************************************************************************************************
-        */
-
         console.log("x: " + x + " y: " + y)
 
-        let iValue = ((this.size-2)*x+(this.size))/2*Math.pow(x,2);
-        let jValue = ((this.size-2)*y+(this.size))/2*Math.pow(y,2);
-
-        let iIncrement = -2*Math.pow(x,2) + 2*x + 1;
-        let jIncrement = -2*Math.pow(y,2) + 2*y + 1;
-
-        for(let i=iValue; i>=0 && i<this.size;i+=iIncrement){
-            for(let j=jValue; j>=0 && j<this.size;j+=jIncrement){
+        for(let i=1*(x<0)+(this.size-1)*(x>0); i>=0 && i<this.size;i+=1*(x<=0)-1*(x>0)){
+            for(let j=1*(y<0)+(this.size-1)*(y>0); j>=0 && j<this.size;j+=1*(y<=0)-1*(y>0)){
                 // console.log("x: " + x + " y: " + y)
                 console.log("i: " + i + " j: " + j);
-                if(this.spaces[i][j].value!=0 && this.spaces[i-x][j-y].value==0){
-                    this.spaces[i-x][j-y] = {...this.spaces[i][j]};
-                    this.spaces[i][j] = new Tile(false);
+                slideX = 0;
+                slideY = 0;
+
+                if(this.spaces[i+x*slideX][j+y*slideY].value!==0 && this.spaces[i+x+x*slideX][j+y+y*slideY].value===0){
+                    this.spaces[i+x+x*slideX][j+y+y*slideY] = {...this.spaces[i+x*slideX][j+y*slideY]};
+                    this.spaces[i+x*slideX][j+y*slideY] = new Tile(false);
+
+                    console.log("xPos: " + (i+x+x*slideX) + " yPos: " + (j+y+y*slideY));
+
+                    slideX-=x;
+                    slideY-=y;
                 }
             }
         }
